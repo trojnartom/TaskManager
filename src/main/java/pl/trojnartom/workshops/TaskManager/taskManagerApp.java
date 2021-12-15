741b5a3f8626953741ac9a01160d20f8a98bfb03
trojnartom <trojnar.t@hotmail.com>
2021-12-15 17:11:29 +0100

    Welcome + Menu

--- src/main/java/pl/trojnartom/workshops/TaskManager/taskManagerApp.java
+++ src/main/java/pl/trojnartom/workshops/TaskManager/taskManagerApp.java
@@ -1,4 +1,39 @@
 package pl.trojnartom.workshops.TaskManager;
 
+
+import static pl.coderslab.ConsoleColors.BLUE;
+
 public class taskManagerApp {
+
+    private static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    private static final String EXIT_OPTION = "exit";
+    private static final String TASKS_FILE_NAME = "tasks.csv";
+
+    private static String[][] tasks = new String[0][];
+
+    public static void main(String[] args) {
+    }
+
+
+    public static void app() {
+        printWelcomeMessage ();
+        Menu();
+
+    }
+
+
+    private static void printWelcomeMessage() {
+        String userName = System.getProperty("user.name");
+        System.out.println("Hello " + userName);
+    }
+
+    private static void Menu () {
+        System.out.println("Available options: ");
+        for (String option: OPTIONS){
+            System.out.println("" + option);
+        }
+        System.out.println(BLUE);
+
+    }
+
 }
